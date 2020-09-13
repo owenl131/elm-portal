9aa83baff30efac8d2b72690dc561ad5786f353c
Owen Leong <owenl131@gmail.com>
2020-09-13 16:45:07 +0800

    Change backend based on new schema

--- backend/server.py
+++ backend/server.py
@@ -1,37 +1,31 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import random
+import string
 
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
 
 app = Flask(__name__)
 CORS(app)
 
-sampleSession1 = {
-    'id': 'session123',
-    'date': '2020-09-10',
-    'remarks': 'Class'
-}
-sampleSession2 = {
-    'id': 'session124',
-    'date': '2020-09-03',
-    'remarks': 'Class'
-}
-sampleClass = {
-    'id': 'class123',
-    'name': 'P4 Math',
-    'timeslot': 'Monday',
-    'year': 2020,
-    'sessions': [
-        sampleSession1,
-        sampleSession2
-    ]
-}
-sampleTutor = {
-    'id': 'tutor123',
-    'name': 'ABC Tan',
-    'classes': [
-        sampleClass
-    ]
-}
+def makeTutor():
+    return {
+        'id': 'id' + get_random_string(10),
+        'name': 'Name ' + get_random_string(4),
+        'email': get_random_string(6) + '@gmail.com',
+        'school': get_random_string(4) +  ' School',
+        'dateOfBirth': '2000-02-01',
+        'dateOfRegistration': '2020-01-15',
+        'gender': 'm',
+        'status': 1,
+        'admin': 0
+    }
+
+tutors = [makeTutor() for _ in range(30)]
+
 
 @app.route('/')
 def hello_world():
@@ -44,7 +38,7 @@ def hello_world():
 
 @app.route('/tutors')
 def getTutors():
-    return jsonify([sampleTutor])
+    return jsonify(tutors[:10])
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000)
