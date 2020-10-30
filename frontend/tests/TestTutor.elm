c7653010ebd58049674f15093206bc9412f188da
Owen Leong <owenl131@gmail.com>
2020-10-31 00:08:14 +0800

    Add first test

---
+++ frontend/tests/TestTutor.elm
@@ -0,0 +1,41 @@
+module TestTutor exposing (..)
+
+import Date
+import Expect
+import Fuzz exposing (Fuzzer, int, list, string)
+import Http exposing (Expect)
+import Json.Decode
+import Json.Encode
+import Test exposing (..)
+import Time
+import Tutor
+
+
+suite : Test
+suite =
+    let
+        tutor : Tutor.Tutor
+        tutor =
+            { id = "abcd1234"
+            , admin = Tutor.LvlTutor
+            , name = "Test Name"
+            , dateOfBirth = Date.fromWeekDate 2000 1 Time.Mon
+            , dateOfRegistration = Date.fromWeekDate 2020 10 Time.Tue
+            , email = "test@email.com"
+            , gender = Tutor.Male
+            , school = "Test school"
+            , status = Tutor.Active
+            , password = Nothing
+            }
+    in
+    test "Decode and encode tutor json"
+        (\_ ->
+            tutor
+                |> Tutor.tutorEncoder
+                |> Json.Encode.encode 2
+                |> Json.Decode.decodeString Tutor.tutorDecoder
+                |> Expect.all
+                    [ Expect.ok
+                    , Result.toMaybe >> Maybe.withDefault Tutor.emptyTutor >> Expect.equal tutor
+                    ]
+        )
